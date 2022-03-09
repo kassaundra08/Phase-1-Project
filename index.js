06c6d56929909783b8dba3dce2bec9761a719da9
Rahul Shah <rashah1998@yahoo.com>
2022-03-09 09:47:35 -0500

    Loaded comments for the top div location, replaced top content on clicks, formatting

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ function renderImages(locations) {
         mainContainer.append(imageContainer);
 
         imageContainer.addEventListener('click', function(e){
+            const removeFromContainer = document.querySelector('#top-container');
+            if(removeFromContainer !== null) {
+                while(removeFromContainer.firstChild) {
+                    removeFromContainer.removeChild(removeFromContainer.firstChild);
+                }
+            }
             const topContainer = document.createElement('div');
             topContainer.id = 'top-container';
             const leftTopContainer = document.createElement('div');
@@ -35,9 +41,7 @@ function renderImages(locations) {
             topLocation.textContent = location.city;
             const topRating = document.createElement('h3');
             topRating.textContent = 'Rating: ' + location.rating + '/10';
-            // console.log(location)
-            // const locationComments = document.querySelector('p');
-            // locationComments.textContent = location.comments
+            loadComments(rightTopContainer, location);
             leftTopContainer.append(topImage)
             rightTopContainer.append(topLocation, topRating)
             topContainer.append(leftTopContainer, rightTopContainer)
@@ -48,3 +52,21 @@ function renderImages(locations) {
 }
 
 
+function loadComments(container, location) {
+    fetch(`${url}/locations/${location.id}`)
+    .then(resp => resp.json())
+    .then(location => {
+        const ul = document.createElement('ul');
+        ul.textContent = 'Comments:';
+        container.append(ul);
+        location.comments.forEach(comment => {
+            renderComment(ul, comment);
+        })
+    })
+}
+
+function renderComment(ul, comment) {
+    const commentToAppend = document.createElement('li');
+    commentToAppend.textContent = `"${comment.content}"`;
+    ul.append(commentToAppend);
+}
